ec6d6b4208033875d8ddd42ac35cca8f638ddfbc
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-08-31 10:30:34 +0200

    First commit

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tamagotchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, It's me");
+        }
+    }
+}
